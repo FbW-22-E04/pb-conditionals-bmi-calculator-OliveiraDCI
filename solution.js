4deb99bf72dc0e0cec7e0be2454ccf15602786ac
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-07-16 14:46:41 +0200

    New branch for bmiFunctions version.

--- solution.js
+++ solution.js
@@ -1,28 +1,20 @@
-//Q1 Store Mark's and John's mass and height in variables.
-let markName = "Mark";
-let johnName = "John";
-let markHeight = 1.6;
-let markMass = 73.5;
-let johnHeight = 1.65;
-let johnMass = 78;
+// BMI Calculator as Functions - development branch 'bmiFunctions'
 
-//Q2 Calculate both their BMIs and store their BMIs in variables.
-const markBMI = markMass / (markHeight * markHeight);
-const johnBMI = johnMass / (johnHeight * johnHeight);
+const calculateBMI = (name, weight, height) => weight * height.toFixed(2);
 
-//Q3 Create a boolean variable containing information about whether Mark has a higher BMI than John.
-const markMoreBeefCake = markBMI > johnBMI;
+const whoIsBiggest = function (
+  name1,
+  weight1,
+  height1,
+  name2,
+  weight2,
+  height2
+) {
+  let markBMI = calculateBMI("Mark", 73.5, 1.6);
+  let johnBMI = calculateBMI("John", 78, 1.65);
+  if (markBMI == johnBMI) return "They have the same BMI.";
+  let biggest = markBMI > johnBMI ? name1 : name2;
+  return `${biggest} is the biggest.`;
+};
 
-//Q4 Print a string to the console containing the variable from step 3 using string interpolation. (e.g. "Is Mark's BMI higher than John's? Why yes, it's [boolean], it is").
-console.log(
-  `Q4: Is Mark's BMI higher than John's? Why yes, it's ${markMoreBeefCake}, it is`
-);
-
-//Q5 Create an *if* statement which prints the name and BMI of the person with the highest BMI.
-if (markBMI > johnBMI) {
-  console.log(`Q5: ${markName} ${markBMI.toFixed(2)}`);
-} else if (johnBMI > markBMI) {
-  console.log(`Q5: ${johnName} ${johnBMI.toFixed(2)}`);
-} else {
-  console.log(`Q5: They both have the same BMI`);
-}
+console.log(whoIsBiggest("Mark", 73.5, 1.6, "John", 78, 1.65));
